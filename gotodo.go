8310f69d5a755b712319b6ba63d4fe254863ba5d
jack-zh <zzh.coder@gmail.com>
2014-12-18 23:35:00 +0800

    add 'add' command

--- gotodo.go
+++ gotodo.go
@@ -4,20 +4,17 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/jack-zh/gotodo/task"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-	"time"
-
-	"github.com/jack-zh/gotodo/task"
 )
 
 var noAct = errors.New("didn't act")
 
 var (
 	file = flag.String("file", defaultFile(".zgotodo", "TODO"), "file in which to store tasks")
-	log  = flag.String("log", defaultFile(".zgotodo-log", "TODOLOG"), "file in which to log removed tasks")
 	now  = flag.Bool("now", false, "when adding, insert at head")
 	done = flag.Bool("done", false, "don't actually add; just append to log file")
 )
@@ -30,17 +27,12 @@ func defaultFile(name, env string) string {
 }
 
 const usage = `Usage:
-	gotodo
-		Show top task
 	gotodo list
 		Show all tasks
-	gotodo N
-		Promote task N to top
-	gotodo rm
-		Remove top task
 	gotodo rm N
 		Remove task N
-Flags:
+	gotodo add ...
+		Add task to list
 `
 
 func main() {
@@ -58,76 +50,22 @@ func main() {
 	case a == "list" && n == 1:
 		var tasks []string
 		tasks, err = list.Get()
-		for i := len(tasks) - 1; i >= 0; i-- {
-			fmt.Printf("%2d: %s\n", i, tasks[i])
+		for i := 0; i < len(tasks); i++ {
+			fmt.Printf("%2d: %s\n", i+1, tasks[i])
 		}
-	case a == "rm" && n <= 2:
+	case a == "rm" && n == 2:
 		i, err2 := strconv.Atoi(flag.Arg(1))
 		if err2 != nil && n == 2 {
 			break
 		}
-		if n == 1 {
-			i = 0
-		}
-		var t string
-		t, err = list.GetTask(i)
-		if err != nil {
-			break
-		}
-		if err2 := logRemovedTask(t); err2 != nil {
-			fmt.Fprintln(os.Stderr, "logging:", err2)
-		}
-		err = list.RemoveTask(i)
+		err = list.RemoveTask(i - 1)
 		if err != nil || n == 2 {
 			break
 		}
-		fallthrough
-	case n == 0:
-		var tasks []string
-		tasks, err = list.Get()
-		if len(tasks) > 0 {
-			fmt.Println(tasks[0])
-		}
-	case n == 1:
-		i, err2 := strconv.Atoi(flag.Arg(0))
-		if err2 != nil {
-			break
-		}
-		var t string
-		t, err = list.GetTask(i)
-		if err != nil {
-			break
-		}
-		err = list.RemoveTask(i)
-		if err != nil {
-			break
-		}
-		err = list.AddTask(t, true)
-		if err == nil {
-			fmt.Println(t)
-		}
+	case a == "add" && n > 1:
+		t := strings.Join(flag.Args()[1:], " ")
+		err = list.AddTask(t, *now)
+	default:
+		fmt.Fprint(os.Stdout, usage)
 	}
-	if err == noAct {
-		t := strings.Join(flag.Args(), " ")
-		if *done {
-			err = logRemovedTask(t)
-		} else {
-			err = list.AddTask(t, *now)
-		}
-	}
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-}
-
-func logRemovedTask(t string) error {
-	f, err := os.OpenFile(*log, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	now := time.Now().Format("1986-02-17")
-	_, err = fmt.Fprintf(f, "%s %s\n", now, t)
-	return err
 }
