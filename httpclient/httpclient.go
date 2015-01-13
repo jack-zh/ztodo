8e0b2646a758162fb2b45d8e5cc0d8dae031a033
jack.zh <zzh.coder@gmail.com>
2015-01-13 14:47:02 +0800

    add httpclient.go

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -1,63 +1,364 @@
 package httpclient
 
 import (
+	"bufio"
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"compress/zlib"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
+	"reflect"
 	"strings"
+	"time"
 )
 
-func HTTPClientGet(url string) (string, error) {
-	resp, err := http.Get(url)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	return body, err
+type Request struct {
+	headers           []headerTuple
+	Method            string
+	Uri               string
+	Body              interface{}
+	QueryString       interface{}
+	Timeout           time.Duration
+	ContentType       string
+	Accept            string
+	Host              string
+	UserAgent         string
+	Insecure          bool
+	MaxRedirects      int
+	RedirectHeaders   bool
+	Proxy             string
+	Compression       *compression
+	BasicAuthUsername string
+	BasicAuthPassword string
 }
 
-func HTTPClientPost(url string, task string) (string, error) {
-	resp, err := http.Post(url,
-		"application/x-www-form-urlencoded",
-		strings.NewReader("task="+task))
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	return body, err
+type compression struct {
+	writer          func(buffer io.Writer) (io.WriteCloser, error)
+	reader          func(buffer io.Reader) (io.ReadCloser, error)
+	ContentEncoding string
 }
 
-func HTTPClientPostForm(url string) (string, error) {
-	resp, err := http.PostForm(url,
-		url.Values{"key": {"Value"}, "id": {"123"}})
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	return body, err
+type Response struct {
+	StatusCode    int
+	ContentLength int64
+	Body          *Body
+	Header        http.Header
 }
 
-func HTTPClientDo(url string, dotype string) (string, error) {
-	client := &http.Client{}
+type headerTuple struct {
+	name  string
+	value string
+}
 
-	// dotype "POST" GET DELETE PUT ...
-	req, err := http.NewRequest(dotype, url, strings.NewReader("name=cjb"))
+type Body struct {
+	reader           io.ReadCloser
+	compressedReader io.ReadCloser
+}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-	req.Header.Set("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
-	req.Header.Set("Accept-Encoding", "gzip,deflate,sdch")
-	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
-	req.Header.Set("Cache-Control", "max-age=0")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("User-Agent", "chrome 100")
-	req.Header.Set("Cookie", "name=anny")
+type Error struct {
+	timeout bool
+	Err     error
+}
 
-	resp, err := client.Do(req)
+func (e *Error) Timeout() bool {
+	return e.timeout
+}
 
-	defer resp.Body.Close()
+func (e *Error) Error() string {
+	return e.Err.Error()
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
+func (b *Body) Read(p []byte) (int, error) {
+	if b.compressedReader != nil {
+		return b.compressedReader.Read(p)
+	}
+	return b.reader.Read(p)
+}
 
-	return body, err
+func (b *Body) Close() error {
+	err := b.reader.Close()
+	if b.compressedReader != nil {
+		return b.compressedReader.Close()
+	}
+	return err
+}
+
+func (b *Body) FromJsonTo(o interface{}) error {
+	if body, err := ioutil.ReadAll(b); err != nil {
+		return err
+	} else if err := json.Unmarshal(body, o); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Body) ToString() (string, error) {
+	body, err := ioutil.ReadAll(b)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func Gzip() *compression {
+	reader := func(buffer io.Reader) (io.ReadCloser, error) {
+		return gzip.NewReader(buffer)
+	}
+	writer := func(buffer io.Writer) (io.WriteCloser, error) {
+		return gzip.NewWriter(buffer), nil
+	}
+	return &compression{writer: writer, reader: reader, ContentEncoding: "gzip"}
+}
+
+func Deflate() *compression {
+	reader := func(buffer io.Reader) (io.ReadCloser, error) {
+		return flate.NewReader(buffer), nil
+	}
+	writer := func(buffer io.Writer) (io.WriteCloser, error) {
+		return flate.NewWriter(buffer, -1)
+	}
+	return &compression{writer: writer, reader: reader, ContentEncoding: "deflate"}
+}
+
+func Zlib() *compression {
+	reader := func(buffer io.Reader) (io.ReadCloser, error) {
+		return zlib.NewReader(buffer)
+	}
+	writer := func(buffer io.Writer) (io.WriteCloser, error) {
+		return zlib.NewWriter(buffer), nil
+	}
+	return &compression{writer: writer, reader: reader, ContentEncoding: "deflate"}
+}
+
+func paramParse(query interface{}) (string, error) {
+	switch query.(type) {
+	case url.Values:
+		return query.(url.Values).Encode(), nil
+	case *url.Values:
+		return query.(*url.Values).Encode(), nil
+	default:
+		var (
+			v = &url.Values{}
+			s = reflect.ValueOf(query)
+			t = reflect.TypeOf(query)
+		)
+		for t.Kind() == reflect.Ptr || t.Kind() == reflect.Interface {
+			s = s.Elem()
+			t = s.Type()
+		}
+		if t.Kind() == reflect.Struct {
+			for i := 0; i < t.NumField(); i++ {
+				if !s.Field(i).CanInterface() {
+					continue
+				}
+				v.Add(strings.ToLower(t.Field(i).Name), fmt.Sprintf("%v", s.Field(i).Interface()))
+			}
+			return v.Encode(), nil
+		} else {
+			return "", errors.New("Can not parse QueryString.")
+		}
+	}
+}
+
+func prepareRequestBody(b interface{}) (io.Reader, error) {
+	switch b.(type) {
+	case string:
+		return strings.NewReader(b.(string)), nil
+	case io.Reader:
+		return b.(io.Reader), nil
+	case []byte:
+		return bytes.NewReader(b.([]byte)), nil
+	case nil:
+		return nil, nil
+	default:
+		j, err := json.Marshal(b)
+		if err == nil {
+			return bytes.NewReader(j), nil
+		}
+		return nil, err
+	}
+}
+
+var defaultDialer = &net.Dialer{Timeout: 1000 * time.Millisecond}
+var defaultTransport = &http.Transport{Dial: defaultDialer.Dial, Proxy: http.ProxyFromEnvironment}
+var defaultClient = &http.Client{Transport: defaultTransport}
+
+var proxyTransport *http.Transport
+var proxyClient *http.Client
+
+func SetConnectTimeout(duration time.Duration) {
+	defaultDialer.Timeout = duration
+}
+
+func (r *Request) AddHeader(name string, value string) {
+	if r.headers == nil {
+		r.headers = []headerTuple{}
+	}
+	r.headers = append(r.headers, headerTuple{name: name, value: value})
+}
+
+func (r Request) Do() (*Response, error) {
+	var req *http.Request
+	var er error
+	var transport = defaultTransport
+	var client = defaultClient
+	var redirectFailed bool
+
+	r.Method = valueOrDefault(r.Method, "GET")
+
+	if r.Proxy != "" {
+		proxyUrl, err := url.Parse(r.Proxy)
+		if err != nil {
+			return nil, &Error{Err: err}
+		}
+		if proxyTransport == nil {
+			proxyTransport = &http.Transport{Dial: defaultDialer.Dial, Proxy: http.ProxyURL(proxyUrl)}
+			proxyClient = &http.Client{Transport: proxyTransport}
+		} else {
+			proxyTransport.Proxy = http.ProxyURL(proxyUrl)
+		}
+		transport = proxyTransport
+		client = proxyClient
+	}
+
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		if len(via) > r.MaxRedirects {
+			redirectFailed = true
+			return errors.New("Error redirecting. MaxRedirects reached")
+		}
+
+		if r.RedirectHeaders {
+			for key, val := range via[0].Header {
+				req.Header[key] = val
+			}
+		}
+		return nil
+	}
+
+	if r.Insecure {
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	} else if transport.TLSClientConfig != nil {
+		transport.TLSClientConfig.InsecureSkipVerify = false
+	}
+
+	b, e := prepareRequestBody(r.Body)
+	if e != nil {
+		return nil, &Error{Err: e}
+	}
+
+	if r.QueryString != nil {
+		param, e := paramParse(r.QueryString)
+		if e != nil {
+			return nil, &Error{Err: e}
+		}
+		r.Uri = r.Uri + "?" + param
+	}
+
+	var bodyReader io.Reader
+	if b != nil && r.Compression != nil {
+		buffer := bytes.NewBuffer([]byte{})
+		readBuffer := bufio.NewReader(b)
+		writer, err := r.Compression.writer(buffer)
+		if err != nil {
+			return nil, &Error{Err: err}
+		}
+		_, e = readBuffer.WriteTo(writer)
+		writer.Close()
+		if e != nil {
+			return nil, &Error{Err: e}
+		}
+		bodyReader = buffer
+	} else {
+		bodyReader = b
+	}
+	req, er = http.NewRequest(r.Method, r.Uri, bodyReader)
+
+	if er != nil {
+		return nil, &Error{Err: er}
+	}
+
+	req.Host = r.Host
+
+	r.addHeaders(req.Header)
+	if r.Compression != nil {
+		req.Header.Add("Content-Encoding", r.Compression.ContentEncoding)
+		req.Header.Add("Accept-Encoding", r.Compression.ContentEncoding)
+	}
+	if r.headers != nil {
+		for _, header := range r.headers {
+			req.Header.Add(header.name, header.value)
+		}
+	}
+
+	if r.BasicAuthUsername != "" {
+		req.SetBasicAuth(r.BasicAuthUsername, r.BasicAuthPassword)
+	}
+
+	timeout := false
+	var timer *time.Timer
+	if r.Timeout > 0 {
+		timer = time.AfterFunc(r.Timeout, func() {
+			transport.CancelRequest(req)
+			timeout = true
+		})
+	}
+
+	res, err := client.Do(req)
+	if timer != nil {
+		timer.Stop()
+	}
+
+	if err != nil {
+		if !timeout {
+			switch err := err.(type) {
+			case *net.OpError:
+				timeout = err.Timeout()
+			case *url.Error:
+				if op, ok := err.Err.(*net.OpError); ok {
+					timeout = op.Timeout()
+				}
+			}
+		}
+
+		var response *Response
+		if redirectFailed {
+			response = &Response{StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body}}
+		}
+
+		return response, &Error{timeout: timeout, Err: err}
+	}
+
+	if r.Compression != nil && strings.Contains(res.Header.Get("Content-Encoding"), r.Compression.ContentEncoding) {
+		compressedReader, err := r.Compression.reader(res.Body)
+		if err != nil {
+			return nil, &Error{Err: err}
+		}
+		return &Response{StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body, compressedReader: compressedReader}}, nil
+	} else {
+		return &Response{StatusCode: res.StatusCode, ContentLength: res.ContentLength, Header: res.Header, Body: &Body{reader: res.Body}}, nil
+	}
+}
+
+func (r Request) addHeaders(headersMap http.Header) {
+	if len(r.UserAgent) > 0 {
+		headersMap.Add("User-Agent", r.UserAgent)
+	}
+	headersMap.Add("Accept", r.Accept)
+	headersMap.Add("Content-Type", r.ContentType)
+}
+
+func valueOrDefault(value, def string) string {
+	if value != "" {
+		return value
+	}
+	return def
 }
---
+++ httpclient/oldhttpclient.go
@@ -0,0 +1,63 @@
+package httpclient
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func HTTPClientGet(url string) (string, error) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
+
+func HTTPClientPost(url string, task string) (string, error) {
+	resp, err := http.Post(url,
+		"application/x-www-form-urlencoded",
+		strings.NewReader("task="+task))
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
+
+func HTTPClientPostForm(url string) (string, error) {
+	resp, err := http.PostForm(url,
+		url.Values{"key": {"Value"}, "id": {"123"}})
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
+
+func HTTPClientDo(url string, dotype string) (string, error) {
+	client := &http.Client{}
+
+	// dotype "POST" GET DELETE PUT ...
+	req, err := http.NewRequest(dotype, url, strings.NewReader("name=cjb"))
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Set("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
+	req.Header.Set("Accept-Encoding", "gzip,deflate,sdch")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
+	req.Header.Set("Cache-Control", "max-age=0")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("User-Agent", "chrome 100")
+	req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+}
