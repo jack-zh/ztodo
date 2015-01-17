a6cf7ff317d518b40fe13d0c57c226e50f9eb4b2
jack.zh <zzh.coder@gmail.com>
2015-01-17 22:33:24 +0800

    add test app

--- zserver/testapp.go
+++ zserver/testapp.go
@@ -5,22 +5,64 @@ import (
 	"github.com/jack-zh/ztodo/zrequests"
 )
 
-func main() {
-	fmt.Println("Hello")
-	type Item struct {
-		UserName string
-		Password string
-	}
+type Item struct {
+	UserName string
+	Password string
+}
 
-	item := Item{UserName: "jack", Password: "123456"}
+func signup() string {
+	return "signup-->"
+}
+
+func login() string {
+	return ""
+}
+
+func getuser(usertokenstr string) string {
+	return "getuser--> usertokenstr" + usertokenstr
+}
+
+func pullall() {
+	// item := Item{UserName: "jack", Password: "123456"}
 	res, err := zrequests.Request{
-		Method:      "POST",
-		Uri:         "http://localhost:9999/login",
-		QueryString: item,
+		Method: "GET",
+		Uri:    "http://localhost:9999/pullall/aaabbbcccddd",
+		// QueryString: item,
 	}.Do()
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res.Body.ToString())
 	}
+}
+
+func pullone() {
+	// item := Item{UserName: "jack", Password: "123456"}
+	res, err := zrequests.Request{
+		Method: "GET",
+		Uri:    "http://localhost:9999/pullall/aaabbbcccddd",
+		// QueryString: item,
+	}.Do()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.Body.ToString())
+	}
+}
+
+func pushall(usertokenstr string) string {
+	return "pushall==> usertokenstr:" + usertokenstr
+}
+
+func pushone(usertokenstr string) string {
+	return "pushone==> usertokenstr:" + usertokenstr
+}
+
+func pnf(url string) string {
+	return "{'error': 404}"
+}
+
+func main() {
+	fmt.Println("Hello")
+
 }
