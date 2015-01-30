55a8fd6a8a1b8a59cc753a787b97de136567b791
jack.zh <zzh.coder@gmail.com>
2015-01-30 16:43:29 +0800

    add color

--- zterminal/terminal/util_bsd.go
+++
@@ -1,8 +0,0 @@
-// +build darwin dragonfly freebsd netbsd openbsd
-
-package terminal
-
-import "syscall"
-
-const ioctlReadTermios = syscall.TIOCGETA
-const ioctlWriteTermios = syscall.TIOCSETA
--- zterminal/terminal/util_linux.go
+++
@@ -1,6 +0,0 @@
-// +build linux
-
-package terminal
-
-const ioctlReadTermios = 0x5401
-const ioctlWriteTermios = 0x5402
